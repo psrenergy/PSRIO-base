291439e6dc93a4b6e1392f742a24104d395153be
raphasampaio <raphaelaraujosampaio@gmail.com>
2021-04-12 16:12:32 -0300

    Fixed sddpcoped

--- sddp-reports/sddpcoped.lua
+++ sddp-reports/sddpcoped.lua
@@ -2,10 +2,13 @@ local function sddpcoped()
     local costs = require("sddp/costs");
     local future_cost = require("sddp/future_cost");
     local discount_rate = require("sddp/discount_rate");
+    
+    local costs_aggregated = costs():aggregate_blocks(BY_SUM()):aggregate_scenarios(BY_AVERAGE());
+    local future_cost_aggregated = future_cost():aggregate_blocks(BY_SUM()):aggregate_scenarios(BY_AVERAGE());
 
     return concatenate(
-        (costs():aggregate_blocks(BY_SUM()):aggregate_scenarios(BY_AVERAGE()) / discount_rate):aggregate_stages(BY_SUM()),
-        (future_cost():aggregate_blocks(BY_SUM())/discount_rate):aggregate_stages(BY_LAST_VALUE()):aggregate_scenarios(BY_AVERAGE())
+        (costs_aggregated / discount_rate()):aggregate_stages(BY_SUM()),
+        (future_cost_aggregated / discount_rate()):aggregate_stages(BY_LAST_VALUE())
     );
 end
 return sddpcoped;
