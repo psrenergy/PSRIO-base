c96200e23c9536df055bf61d35545e6ae877858a
Luiz Carlos da Costa Junior <luizcarlos@psr-inc.com>
2021-06-16 21:44:19 -0300

    Fixed defcit_risk

--- sddp/defcit_risk.lua
+++ sddp/defcit_risk.lua
@@ -1,6 +1,6 @@
 local function defcit_risk(suffix)
     local system = require("collection/system");
     local defcit = system:load("defcit" .. (suffix or ""));
-    return ifelse(defcit:aggregate_stages(BY_SUM(), Profile.PER_YEAR):gt(0), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%");
+    return ifelse(defcit:aggregate_blocks(BY_SUM()):aggregate_stages(BY_SUM(), Profile.PER_YEAR):gt(0), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%");
 end
 return defcit_risk;
