acd0defd3c7fcd2a62941ca0e6a7df88f19d4740
rsampaio <rsampaio@psr-inc.com>
2021-08-25 16:35:56 -0300

    Renamed thermal.germax to thermal.capacity

--- sddp/potter.lua
+++ sddp/potter.lua
@@ -1,5 +1,5 @@
 local function potter()
     local thermal = Thermal();
-    return ifelse(thermal.existing:gt(0.5), 0, thermal.germax_maintenance);
+    return ifelse(thermal.existing:gt(0.5), 0, thermal.capacity_maintenance);
 end
 return potter;
