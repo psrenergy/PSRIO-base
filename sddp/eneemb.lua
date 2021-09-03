bb6cad2526ca212d7f4c17cc9f646e86f8671839
rsampaio <rsampaio@psr-inc.com>
2021-09-03 15:24:46 -0300

    Updated eneemb and enembp

--- sddp/eneemb.lua
+++ sddp/eneemb.lua
@@ -1,7 +1,8 @@
 local function eneemb(suffix)
     local hydro = Hydro();
+    local reservoirs = hydro.vmax:gt(hydro.vmin);
     local volfin = hydro:load("volfin" .. (suffix or ""));
     local fprodtac = hydro:load("fprodtac" .. (suffix or ""));
-    return ifelse(hydro.existing:gt(0.5), 0, max(0, volfin - hydro.vmin) * fprodtac):convert("GWh");
+    return ifelse(hydro.existing:gt(0.5), 0, max(0, volfin - hydro.vmin) * fprodtac):select_agents(reservoirs):convert("GWh");
 end
 return eneemb;
--- sddp/enembp.lua
+++ sddp/enembp.lua
@@ -1,7 +1,8 @@
 local function enembp(suffix)
     local hydro = Hydro();
+    local reservoirs = hydro.vmax:gt(hydro.vmin);
     local eneemb = hydro:load("eneemb" .. (suffix or ""));
     local eembmx = hydro:load("eembmx" .. (suffix or ""));
-    return (eneemb / eembmx):convert("%");
+    return ifelse(eembmx:ne(0), eneemb / eembmx, 0):select_agents(reservoirs):convert("%");
 end
 return enembp;
