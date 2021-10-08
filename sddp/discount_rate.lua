840a446ed760117e6503a4ebf3a191ffb0cead3b
rsampaio <rsampaio@psr-inc.com>
2021-10-08 18:47:38 -0300

    Updated discount_rate

--- sddp/discount_rate.lua
+++ sddp/discount_rate.lua
@@ -1,5 +1,5 @@
 local function discount_rate()
     local study = Study();
-    return (1 + study.discount_rate) ^ ((study.stage - 1) / study.stages_per_year);
+    return (1 + study.discount_rate) ^ ((study.stage - 1) / study:stages_per_year());
 end
 return discount_rate;
