1a61787cc22e6800a171e972970124dcda724c67
rsampaio <rsampaio@psr-inc.com>
2021-10-13 20:41:44 -0300

    Updated losses.lua

--- sddp/losses.lua
+++ sddp/losses.lua
@@ -1,7 +1,7 @@
 local function losses(suffix)
     local circuit = Circuit();
-    local r =  circuit.resistance;
-    local f = cirucit:load("cirflw");
+    local r =  circuit.resistance:convert("pu");
+    local f = (circuit:load("cirflw") / 100):force_unit("pu");
     return r * (f^2);
 end
 return losses;
