6ccf36d55df76ca71fcf40a8e26c090504bf13a5
raphasampaio <rsampaio@psr-inc.com>
2021-11-05 18:49:15 -0300

    Fixed violations outputs

--- sddp/maximum_spillage_unit_violation_cost.lua
+++ sddp/maximum_spillage_unit_violation_cost.lua
@@ -2,6 +2,6 @@ local function maximum_spillage_unit_violation_cost(suffix)
     local hydro = Hydro();
     local uvcmxsp = hydro:load("uvcmxsp" .. (suffix or ""));
     local vimxsp = hydro:load("vimxsp" .. (suffix or ""));
-    return uvcmto * vimxsp;
+    return uvcmxsp * vimxsp;
 end
 return maximum_spillage_unit_violation_cost;
--- sddp/minimum_spillage_percentage_violation_cost.lua
+++ sddp/minimum_spillage_percentage_violation_cost.lua
@@ -2,6 +2,6 @@ local function minimum_spillage_percentage_violation_cost(suffix)
     local hydro = Hydro();
     local ucvmnbiosp = hydro:load("ucvmnbiosp" .. (suffix or ""));
     local vimnbiosp = hydro:load("vimnbiosp" .. (suffix or ""));
-    return uvcmto * vimnbiosp;
+    return ucvmnbiosp * vimnbiosp;
 end
 return minimum_spillage_percentage_violation_cost;
