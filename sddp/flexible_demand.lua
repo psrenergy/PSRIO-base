a5263c953cd8522472f0c8c71c6dce9b3b6f9f01
raphasampaio <rsampaio@psr-inc.com>
2022-06-22 15:30:52 -0300

    Added flexible_demand

---
+++ sddp/flexible_demand.lua
@@ -0,0 +1,12 @@
+local function flexible_demand()
+    local demand = Demand();
+    local study = Study();
+    local system = System();
+
+    if study:is_hourly_load() then
+        return (demand.inelastic_hour * system.sensitivity):select_agents(demand.is_flexible);
+    else
+        return (demand.inelastic_block * system.sensitivity):select_agents(demand.is_flexible);
+    end
+end
+return flexible_demand;
