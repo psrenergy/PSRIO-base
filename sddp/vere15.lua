5e77f48ff737ab013ee928e44080375d3e154068
Bruno Amaral <brunoamaral@psr-inc.com>
2022-09-26 15:42:54 -0300

    Fix suffix usage in vere15

--- sddp/vere15.lua
+++ sddp/vere15.lua
@@ -4,8 +4,8 @@ local function vere15(suffix)
 
     local tolerance = 0.015;
 
-    local deficit = system:load("defcit" .. suffix):aggregate_blocks(BY_SUM());
-    local supplied_load = demand:load("demandel" .. suffix):aggregate_blocks(BY_SUM());
+    local deficit = system:load("defcit" .. (suffix or "")):aggregate_blocks(BY_SUM());
+    local supplied_load = demand:load("demandel" .. (suffix or "")):aggregate_blocks(BY_SUM());
 
     local percentage = ifelse(supplied_load:ne(0.0), deficit / supplied_load, 0.0);
     local scenarios = ifelse(percentage:gt(tolerance), 1, 0);
