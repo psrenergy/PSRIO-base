3641cc3bc5f9e90a9f814e4aade66c3ff6a88f44
raphasampaio <rsampaio@psr-inc.com>
2022-09-26 11:16:26 -0300

    Updated vere15

--- sddp/vere15.lua
+++ sddp/vere15.lua
@@ -1,28 +1,16 @@
-local function vere15()
-    -- Loading collections
-    sys = System();
-    demand = Generic();
-    
-    -- Tolerance
-    tol = 0.015 -- %
-    
-    -- Load deficit file system
-    deficit       = sys:load("defcit");
-    supplied_load = demand:load("demandel");
-    
-    -- Aggregating block data
-    agg_stg_supplied_load = supplied_load:aggregate_blocks(BY_SUM());
-    agg_stg_defcit = deficit:aggregate_blocks(BY_SUM());
-    
-    -- Count scenarios that have deficit above tol%
-    agg_stg_pcent_defcit = ifelse(agg_stg_supplied_load:ne(0.0),agg_stg_defcit/agg_stg_supplied_load,0.0);
-    abv_tol_pcent_defcit_bin = ifelse(agg_stg_pcent_defcit:gt(tol),1,0);
-    agg_stg_pcent_defcit = agg_stg_pcent_defcit*abv_tol_pcent_defcit_bin
-    
-    -- Calculate vere15[%]
-    agg_abv_tol_defcit_count = abv_tol_pcent_defcit_bin:aggregate_scenarios(BY_SUM());
-    output = ifelse(agg_abv_tol_defcit_count:ne(0.0),agg_stg_pcent_defcit:aggregate_scenarios(BY_SUM())/agg_abv_tol_defcit_count,0.0):convert("%");
-    
-    return output
+local function vere15(suffix)
+    local system = System();
+    local demand = Generic();
+
+    local tolerance = 0.015; -- %
+
+    local deficit = system:load("defcit" .. suffix):aggregate_blocks(BY_SUM());
+    local supplied_load = demand:load("demandel" .. suffix):aggregate_blocks(BY_SUM());
+
+    local percentage = ifelse(supplied_load:ne(0.0), deficit / supplied_load, 0.0);
+    local scenarios = ifelse(percentage:gt(tolerance), 1, 0);
+    local count = scenarios:aggregate_scenarios(BY_SUM());
+
+    return ifelse(count:ne(0.0), (percentage * scenarios):aggregate_scenarios(BY_SUM()) / count, 0.0):convert("%");
 end
-return vere15
+return vere15;
