c20a77d83b703ee089c6209517dbde7ede2268f6
raphasampaio <rsampaio@psr-inc.com>
2022-12-19 14:35:22 -0300

    Added min_flexible_load and max_flexible_load

---
+++ sddp/max_flexible_load.lua
@@ -0,0 +1,7 @@
+local function max_flexible_load()
+    local demand = Demand();
+    local flexible_demand = require("sddp/flexible_demand");
+
+    return flexible_demand() * (1 + demand.max_increase:select_agents(demand.is_flexible));
+end
+return max_flexible_load;
