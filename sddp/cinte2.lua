18185a3c8f763f646e0beb4e9866966c91f6554c
raphasampaio <rsampaio@psr-inc.com>
2022-12-20 01:16:23 -0300

    Minor changes

--- sddp/cinte2.lua
+++ sddp/cinte2.lua
@@ -2,6 +2,6 @@ local function cinte2(i)
     local thermal = Thermal(i or 1);
     local fuel = Fuel(i or 1);
 
-    return (thermal.specific_consumption_segment_2 * (thermal.fuel_transportation_cost + fuel.cost) + thermal.om_cost);
+    return thermal.specific_consumption_segment_2 * (thermal.fuel_transportation_cost + fuel.cost) + thermal.om_cost;
 end
 return cinte2;
--- sddp/cinte3.lua
+++ sddp/cinte3.lua
@@ -2,6 +2,6 @@ local function cinte3(i)
     local thermal = Thermal(i or 1);
     local fuel = Fuel(i or 1);
 
-    return (thermal.specific_consumption_segment_3 * (thermal.fuel_transportation_cost + fuel.cost) + thermal.om_cost);
+    return thermal.specific_consumption_segment_3 * (thermal.fuel_transportation_cost + fuel.cost) + thermal.om_cost;
 end
 return cinte3;
