6da6ab31632b55fa43a54186f3d58a76338fe170
raphasampaio <rsampaio@psr-inc.com>
2022-12-29 03:02:45 -0300

    Renamed to thermal_generation_running_hours

---
+++ sddp/thermal_generation_running_hours.lua
@@ -0,0 +1,10 @@
+local function thermal_generation_running_hours(i, suffix)
+    local system = System(i or 1);
+    local thermal = Thermal(i or 1);
+
+    local thermal_generation = thermal:load("gerter" .. (suffix or ""));
+    local has_generation = ifelse(thermal_generation:gt(0), 1, 0);
+
+    return (has_generation * system.load_level_length):aggregate_blocks(BY_SUM()):convert("hours");
+end
+return thermal_generation_running_hours;
