de114b8ea2b02c9d7d2919d22c58d8ff6908d5bf
raphasampaio <rsampaio@psr-inc.com>
2023-02-01 11:15:23 -0300

    Updated hydro_balance

--- sddp/hydro_balance.lua
+++ sddp/hydro_balance.lua
@@ -8,7 +8,7 @@ local function hydro_balance(i, suffix)
     local irrigation = hydro:load("qriego" .. (suffix or "")):convert("hm3");
     local final_storage = hydro:load("volfin" .. (suffix or ""));
 
-    local arrived_spillage = spillage:aggregate_topology(BY_SUM(), Topology.TURBINED_FROM, 1, 1);
+    local arrived_spillage = spillage:aggregate_topology(BY_SUM(), Topology.SPILLED_FROM, 1, 1);
     local arrived_turbining = turbining:aggregate_topology(BY_SUM(), Topology.TURBINED_FROM, 1, 1);
     local delta_final_storage = final_storage:uncouple_stages(true);
 
