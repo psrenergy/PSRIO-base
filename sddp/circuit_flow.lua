697f9500d5c99d8fa4846d41e2bbb2ecaa84bcf1
raphasampaio <rsampaio@psr-inc.com>
2023-05-04 17:41:45 -0300

    Added circuit_flow

---
+++ sddp/circuit_flow.lua
@@ -0,0 +1,10 @@
+local function circuit_flow(i, suffix)
+    local bus = Bus(i or 1);
+    local circuit = Circuit(i or 1);
+
+    local bus_angle = bus:load("angulo" .. (suffix or "")):convert("radian");
+    local susceptance = 1 / circuit.reactance:convert("pu");
+
+    return (100 * susceptance * (bus_angle:circuit_from() - bus_angle:circuit_to())):force_unit("MW");
+end
+return circuit_flow;
