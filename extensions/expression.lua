24e068885662bd359c657d894bcc5cedd11fd512
raphasampaio <rsampaio@psr-inc.com>
2023-05-22 20:37:50 -0300

    Added extensions

--- extensions/expression.lua
+++ extensions/expression.lua
@@ -1,4 +1,32 @@
+function Expression.add_prefix(self, prefix)
+    if prefix == nil then
+        error("Prefix must not be nil");
+    end
+
+    local agents = self:agents();
+    for i, agent in ipairs(agents) do
+        agents[i] = prefix .. agent;
+    end
+    return self:rename_agents(agents);
+end
+
+function Expression.add_suffix(self, suffix)
+    if suffix == nil then
+        error("Suffix must not be nil");
+    end
+
+    local agents = self:agents();
+    for i, agent in ipairs(agents) do
+        agents[i] = agent .. suffix;
+    end
+    return self:rename_agents(agents);
+end
+
 function Expression.aggregate_agents_by_label(self, aggregation)
+    if aggregation == nil then
+        error("Aggregation must not be nil");
+    end
+
     local spairs = require("lua/spairs");
     local agents = self:agents();
 
@@ -19,4 +47,70 @@ function Expression.aggregate_agents_by_label(self, aggregation)
     end
 
     return concatenate(unique_agents);
+end
+
+function Expression.clamp(self, low, hi)
+    if low == nil then
+        error("Low must not be nil");
+    end
+    if hi == nil then
+        error("Hi must not be nil");
+    end
+    return min(max(self, low), hi);
+end
+
+function Expression.remove_agent(self, agent)
+    if agent == nil then
+        error("Agent must not be nil");
+    end
+    return self:remove_agents({ agent });
+end
+
+function Expression.remove_zeros(self)
+    return self:select_agents(self:ne(0));
+end
+
+function Expression.rename_agent(self, agent)
+    if agent == nil then
+        error("Agent must not be nil");
+    end
+    return self:rename_agents({ agent });
+end
+
+function Expression.rename_agents_with_codes(self)
+    local agents = {};
+    for i, code in ipairs(self:codes()) do
+        table.insert(agents, tostring(code));
+    end
+    return self:rename_agents(agents);
+end
+
+function Expression.remove_agent_by_code(self, code)
+    if code == nil then
+        error("Code must not be nil");
+    end
+
+    return self:remove_agents_by_code({ code });
+end
+
+function Expression.select_agent(self, agent)
+    if agent == nil then
+        error("Agent must not be nil");
+    end
+    return self:select_agents({ agent });
+end
+
+function Expression.select_stage(self, stage)
+    if stage == nil then
+        error("Stage must not be nil");
+    end
+    return self:select_first_stage(stage):select_last_stage(stage);
+end
+
+function Expression.select_agent_by_code(self, code)
+    if code == nil then
+        error("Code must not be nil");
+    end
+
+    return self:select_agents_by_code({ code });
 end
--- extensions/tab.lua
+++ extensions/tab.lua
@@ -1,4 +1,8 @@
 function Tab.push_table(self, input)
+    if input == nil then
+        error("Input must not be nil");
+    end
+
     if type(input) ~= "table" then
         error("Input must be a table");
     end
