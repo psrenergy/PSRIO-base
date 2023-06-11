ef091aae1d45b51bcc2896e65f461d4be6671236
Raphael Sampaio <rsampaio@psr-inc.com>
2023-06-11 01:09:22 -0300

    Update costs.lua

--- sddp/costs.lua
+++ sddp/costs.lua
@@ -1,12 +1,12 @@
 local function costs(i, suffix)
     local generic = Generic(i or 1);
-    local objcop = generic:load("objcop" .. (suffix or ""));
+    local costs_by_category = generic:load("objcop" .. (suffix or ""));
 
     local study = Study(i or 1);
     if study:is_hourly() then
-        return objcop:remove_agent(1); -- remove future cost
+        return costs_by_category:remove_agent(1); -- remove future cost
     else
-        return objcop:remove_agents({ 1, -1 }); -- remove total cost and future cost
+        return costs_by_category:remove_agents({ 1, -1 }); -- remove total cost and future cost
     end
 end
 return costs;
