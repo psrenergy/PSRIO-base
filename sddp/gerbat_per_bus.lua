8beeea2122c2819449b51bcc46820f56e48f8da7
raphasampaio <rsampaio@psr-inc.com>
2023-06-23 10:45:37 -0300

    Fixed gerbat_per_bus

--- sddp/gerbat_per_bus.lua
+++ sddp/gerbat_per_bus.lua
@@ -1,5 +1,5 @@
 local function gerbat_per_bus(i, suffix)
-    local battery_generation_per_bus = require("sddp/gerbat_per_bus");
+    local battery_generation_per_bus = require("sddp/battery_generation_per_bus");
 
     return battery_generation_per_bus(i, suffix);
 end
