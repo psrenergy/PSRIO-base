e475934eeea396002fa777fbc86da8be403fb284
raphasampaio <rsampaio@psr-inc.com>
2023-11-10 23:40:55 -0300

    Updated collection.lua

--- extensions/collection.lua
+++ extensions/collection.lua
@@ -1,296 +1,120 @@
-function Area.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+local function load_ncp(collection, filename)
+    local index = collection:get_study_index();
+    local study = Study(index);
+
+    local initial_date = study:get_parameter("ShortTerm", "InitialDate", "");
+    local year, month, day = string.match(initial_date, "(%d+)-(%d+)-(%d+)");
+
+    local epoch = datetime.epoch_from_date(year, month, day);
+    local year_hour = datetime.year_hour_from_epoch(epoch);
+
+    return collection:load(filename):set_initial_stage(year_hour);
 end
 
-function BalancingArea.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Area.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function BalancingAreaHydro.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Battery.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function BalancingAreaThermal.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Bus.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function Battery.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Circuit.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function Bus.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function CircuitsSum.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function Circuit.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function ConcentratedSolarPower.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function CircuitsSum.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function DCLink.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function ConcentratedSolarPower.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Demand.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function DCLink.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Fuel.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function Demand.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function FuelConsumption.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function DemandSegment.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function FuelContract.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function ExpansionCapacity.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function FuelReservoir.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function ExpansionProject.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Generator.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function Fuel.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function GenerationConstraint.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function FuelConsumption.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Generic.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function FuelContract.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Hydro.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function FuelReservoir.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function HydroGaugingStation.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function Generator.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Interconnection.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function GenerationConstraint.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function InterconnectionSum.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function Generic.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function PowerInjection.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function Hydro.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Renewable.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function HydroGaugingStation.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function RenewableGaugingStation.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function Interconnection.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function ReserveGenerationConstraint.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function InterconnectionSum.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function ReservoirSet.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function PowerInjection.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Study.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function Renewable.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function System.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
 
-function RenewableGaugingStation.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
-end
-
-function ReserveGenerationConstraint.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
-end
-
-function ReservoirSet.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
-end
-
-function Study.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
-end
-
-function System.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
-end
-
-function Thermal.load_ncp(self, name)
-    local study = Study()
-    local date = study:get_parameter("ShortTerm", "InitialDate", "o")
-    local year, month, day = string.match(date, "(%d+)-(%d+)-(%d+)")
-    local epoch = datetime.epoch_from_date(year, month, day)
-    local year_hour = datetime.year_hour_from_epoch(epoch)
-    return self:load(name):set_initial_stage(year_hour)
+function Thermal.load_ncp(self, filename)
+    return load_ncp(self, filename);
 end
