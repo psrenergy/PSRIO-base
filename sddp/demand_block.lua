909cddd23a49c8c9a796a6149c9043a5a4b8f881
Iury <iurysaboia927@gmail.com>
2024-06-18 18:43:08 -0300

    Update in demand_block and demand_hour functions

--- sddp/demand_block.lua
+++ sddp/demand_block.lua
@@ -2,6 +2,7 @@ local function demand_block(i)
     local demand<const> = Demand(i or 1);
     local system<const> = System(i or 1);
 
-    return (system.sensitivity * demand.inelastic_block:aggregate_agents(BY_SUM(), Collection.SYSTEM));
+    return (system.sensitivity * (demand.inelastic_block * demand.is_enabled:eq(0))):aggregate_agents(BY_SUM(),
+    Collection.SYSTEM):convert("GWh");
 end
 return demand_block;
--- sddp/demand_hour.lua
+++ sddp/demand_hour.lua
@@ -2,6 +2,7 @@ local function demand_hour(i)
     local demand<const> = Demand(i or 1);
     local system<const> = System(i or 1);
 
-    return (system.sensitivity * demand.inelastic_hour:aggregate_agents(BY_SUM(), Collection.SYSTEM)):convert("GWh");
+    return (system.sensitivity * (demand.inelastic_hour * demand.is_enabled:eq(0))):aggregate_agents(BY_SUM(),
+        Collection.SYSTEM):convert("GWh");
 end
 return demand_hour;
