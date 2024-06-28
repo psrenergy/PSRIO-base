d628025e44df40d53ac645bacacb63a1ef7d706b
Iury <iurysaboia927@gmail.com>
2024-06-28 18:27:29 -0300

    Fix usedcl

--- sddp/usedcl.lua
+++ sddp/usedcl.lua
@@ -4,10 +4,10 @@ local function usedcl(i, suffix)
     local dclink_flow = dclink:load("dclink" .. (suffix or "")):convert("MW");
     local flow_direction = dclink_flow:gt(0);
 
-    return ifelse(
+    return ifelse( 
         flow_direction,
-        ifelse(dclink.capacity_from:gt(0), dclink_flow / dclink.capacity_from, 1),
-        ifelse(dclink.capacity_to:gt(0), -dclink_flow / dclink.capacity_to, 1)
+        safe_divide( dclink_flow, dclink.capacity_from),
+        safe_divide(-dclink_flow, dclink.capacity_to)
     ):convert("%");
 end
 return usedcl;
