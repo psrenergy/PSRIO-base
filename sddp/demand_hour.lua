9714eaf7e0cd76ce6d780bb7e5707a9ce8d904cb
Iury <iurysaboia927@gmail.com>
2024-07-01 10:08:04 -0300

    Update the demand input load to get all demand declared in the interface considering the enable button.

--- sddp/demand_hour.lua
+++ sddp/demand_hour.lua
@@ -2,7 +2,7 @@ local function demand_hour(i)
     local demand<const> = Demand(i or 1);
     local system<const> = System(i or 1);
 
-    return (system.sensitivity * (demand.inelastic_hour * demand.is_enabled:eq(0))):aggregate_agents(BY_SUM(),
+    return (system.sensitivity * (demand.inelastic_hour * demand.is_enabled:eq(0):select_stage(1))):aggregate_agents(BY_SUM(),
         Collection.SYSTEM):convert("GWh");
 end
 return demand_hour;
