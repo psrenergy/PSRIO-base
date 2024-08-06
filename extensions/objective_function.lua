70d7be2c78ae49085fb52bcb5f58628abc41c3f1
Iury <iurysaboia927@gmail.com>
2024-08-06 18:03:49 -0300

    Updated

--- extensions/objective_function.lua
+++ extensions/objective_function.lua
@@ -14,14 +14,15 @@ function ObjectiveFunction.new()
   return self;
 end
 
-function ObjectiveFunction:cost(self, model)
+function ObjectiveFunction:cost(model)
     local model = "SDDP" or model;
 
+    local generic = Generic();
     local output_name;
     if model == "OPTGEN" then
         output_name = "opt1_dashboard_totalcosts";
     else
         output_name = "objcop";
     end
-    return self:load(output_name);
+    return generic:load(output_name);
 end
