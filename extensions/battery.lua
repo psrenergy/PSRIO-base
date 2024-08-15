affb9a2b2f5ed56172d4cba39cfa3a8792440cc1
Iury <iurysaboia927@gmail.com>
2024-08-15 08:36:29 -0300

    Add Battery final_storage function

--- extensions/battery.lua
+++ extensions/battery.lua
@@ -6,3 +6,11 @@ function Battery.generation(self, model)
   return self:load(output_name);
 
 end
+
+function Battery.final_storage(self, model)
+  local model = "SDDP" or model;
+
+  local output_name = "batstg";
+
+  return self:load(output_name);
+end
