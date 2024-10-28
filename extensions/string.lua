5ef76dc96a0e452506adf3b577145f1bb46c0b74
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-10-28 17:46:11 -0300

    Project updates

--- extensions/string.lua
+++ extensions/string.lua
@@ -1,11 +1,10 @@
 function string.random(length)
     local charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    math.randomseed(os.clock());
 
     local vector = {};
     for _ = 1, length do
         local i = math.random(1, #charset);
-        table.insert(vector, charset[i]);
+        table.insert(vector, charset:sub(i, i));
     end
     return table.concat(vector);
 end
--- sddp/usedcl.lua
+++ sddp/usedcl.lua
@@ -4,10 +4,10 @@ local function usedcl(i, suffix)
     local dclink_flow = dclink:load("dclink" .. (suffix or "")):convert("MW");
     local flow_direction = dclink_flow:gt(0);
 
-    return ifelse( 
+    return ifelse(
         flow_direction,
-        safe_divide( dclink_flow, dclink.capacity_from),
-        safe_divide( dclink_flow, dclink.capacity_to)
+        safe_divide(dclink_flow, dclink.capacity_from),
+        safe_divide(dclink_flow, dclink.capacity_to)
     ):convert("%");
 end
 return usedcl;
