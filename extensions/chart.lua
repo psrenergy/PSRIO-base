22711808377133bfb24a9c20d3d90fc81a41e45d
Iury <iurysaboia927@gmail.com>
2024-11-29 19:11:40 -0300

    add_box_plot_categories

--- extensions/chart.lua
+++ extensions/chart.lua
@@ -50,8 +50,8 @@ function Chart.add_categories(self, e1, label, options) --TODO: isso vai morrer
     self:add_category("column", e1, label, options);
 end
 
-function Chart.add_box_plot_categories(self, e1, e2, e3, e4, e5, options)
-    self:add_category("box_plot", e1, e2, e3, e4, e5, options);
+function Chart.add_box_plot_categories(self, e1, e2, e3, e4, e5, label, options)
+    self:add_category("box_plot", e1, e2, e3, e4, e5, label, options);
 end
 
 function Chart.add_column_stacking(self, e1, options)
