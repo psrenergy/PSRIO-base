2f8b9fe7b129343bc0e483488bc1ad56e98b2ff3
Iury <iurysaboia927@gmail.com>
2025-02-06 12:57:00 -0300

    fix demand_hour and demand_block

--- sddp/demand_hour.lua
+++ sddp/demand_hour.lua
@@ -1,9 +1,8 @@
 local function demand_hour(i)
     local demand<const> = Demand(i or 1);
     local system<const> = System(i or 1);
-    local study<const>  = Study(i or 1);
 
-    local initial_stage = study:initial_stage();
+    local initial_stage = 1;
     return (system.sensitivity * (demand.inelastic_hour * demand.is_enabled:eq(0):select_stage(initial_stage)):aggregate_agents(BY_SUM(),
     Collection.SYSTEM)):convert("GWh");
 end
