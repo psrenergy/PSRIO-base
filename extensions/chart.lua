dacb43f91bae4cf4fad97e1db312d766d736a82c
Iury <iurysaboia927@gmail.com>
2025-11-03 15:02:07 -0300

    Improvements in add_block_categories

--- extensions/chart.lua
+++ extensions/chart.lua
@@ -6,9 +6,14 @@ function Chart.add_line_categories(self, e1, label, options)
     self:add_category("line", e1, label, options);
 end
 
-function Chart.add_block_categories(self, e1, options)
+function Chart.add_line_block_categories(self, e1, options)
     options = (options or {});
     self:add_block_category("line", e1, options);
+end
+
+function Chart.add_column_block_categories(self, e1, options)
+    options = (options or {});
+    self:add_block_category("column", e1, options);
 end
 
 function Chart.add_line_exclude_zeros(self, e1, options)
