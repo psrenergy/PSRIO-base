5901b4ba50384bca3f9046e6c5c465593503f76f
Iurysab <87725196+Iurysab@users.noreply.github.com>
2026-01-09 14:09:37 -0300

    Remove year validation for input date
    Removed validation for first year exceeding final year.

--- sddp/calc_pld.lua
+++ sddp/calc_pld.lua
@@ -31,9 +31,6 @@ local function calc_pld(data_name, i)
         if (first_year < initial_year) then
             error(data_name .. " first input date year is before the initial year of cmgdem data.");
         end
-        if (first_year > final_year) then
-            error(data_name .. " input date year exceeds the final year of cmgdem data.");
-        end
         
         -- Get the number of stages in cmgdem data
         local data_stages = cmgdem:stages();
