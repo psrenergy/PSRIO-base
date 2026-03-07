5765d5c4e85411a58e8f6229088b7d59b7e080b1
Iury <iurysaboia927@gmail.com>
2026-03-07 03:15:39 -0300

    Update demand_hour_scenarios method

--- sddp/demand_hour_scenarios.lua
+++ sddp/demand_hour_scenarios.lua
@@ -9,17 +9,24 @@ local function demand_hour_scenarios(i)
 
 
     local initial_stage = 1;
-    local base_demand = (demand_segment.hour * demand.is_enabled:eq(0):select_stage(initial_stage))
+    local base_demand = (demand_segment.hour * demand.is_enabled:eq(0):select_stage(initial_stage));
+    local base_demand_scn = (demand_segment.hour_scenarios * demand.is_enabled:eq(0):select_stage(initial_stage));
     local final_demand = 0;
 
     local demamnd_segment_det = base_demand:select_agents(Collection.DEMAND, {hourly_det_demand}):select_agents(Collection.DEMAND_SEGMENT);
+    local demamnd_segment_scn = base_demand_scn:select_agents(Collection.DEMAND, {hourly_scn_demand}):select_agents(Collection.DEMAND_SEGMENT);
 
-    if #hourly_scn_demand > 0 then
-        local base_demand_scn = (demand_segment.hour_scenarios * demand.is_enabled:eq(0):select_stage(initial_stage))
-        local demamnd_segment_scn = base_demand_scn:select_agents(Collection.DEMAND, {hourly_scn_demand}):select_agents(Collection.DEMAND_SEGMENT);
-        final_demand = demamnd_segment_scn + demamnd_segment_det;
-    else
+    if #hourly_scn_demand > 0 and #hourly_det_demand <= 0 then
+        final_demand = demamnd_segment_scn;
+    elseif #hourly_scn_demand <= 0 and #hourly_det_demand > 0 then
         final_demand = demamnd_segment_det;
+    else
+        if demamnd_segment_scn:stages() > demamnd_segment_det:stages() then
+            demamnd_segment_det = demamnd_segment_scn:select_agent(1):fill(0) + demamnd_segment_det;
+        elseif demamnd_segment_scn:stages() < demamnd_segment_det:stages() then
+            demamnd_segment_scn = demamnd_segment_det:select_agent(1):fill(0) + demamnd_segment_scn;
+        end
+        final_demand = demamnd_segment_scn + demamnd_segment_det;
     end
     
     local hourly_demand = (system.sensitivity * final_demand):convert("GWh"):save_cache();
